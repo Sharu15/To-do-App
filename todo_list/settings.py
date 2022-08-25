2a9e29875cb1801c457ab5a00563ee9f030bd162
Sharu15 <sharanyasharu15blr@gmail.com>
2022-08-25 20:48:12 +0530

    thirl

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'todo_list.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'/templates/base'),],
+        'DIRS': [os.path.join(BASE_DIR,'Templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
