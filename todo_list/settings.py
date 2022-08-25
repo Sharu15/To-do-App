3490123319d0496d484357d3c5d06a9200bd22c4
Sharu15 <sharanyasharu15blr@gmail.com>
2022-08-25 18:07:08 +0530

    second

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%d_ovwgoo_w!l(=)k6c6k(d12ra7(h0l!qnfbb6e$k-hv37b8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -128,6 +128,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'staticfiles'))
 
 django_heroku.settings(locals())
